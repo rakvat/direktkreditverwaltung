bb39957c70f5b80328786d4337e151dc8f43de31
rakvat <rakvat@users.noreply.github.com>
2020-07-30 00:35:48 +0200

    experiments with forms

--- dkapp/urls.py
+++ dkapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
     path('contacts/<int:pk>/', views.ContactView.as_view(), name='contact'),
+    path('contacts/new/', views.contact2, name='contact2'),
 ]
--- dkapp/views.py
+++ dkapp/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Contact
+from .forms import ContactForm
 
 
 class IndexView(generic.TemplateView):
@@ -18,13 +19,34 @@ class ContactsView(generic.ListView):
         return Contact.objects.order_by('last_name', 'first_name')
 
 
+def contact2(request):
+    form = ContactForm()
+    return render(request, 'form.html', {'form': form})
+
+
 class ContactView(generic.DetailView):
     model = Contact
     template_name = 'contacts/detail.html'
 
-    def post(self, *args, **kwargs):
+    def get(self, *args, **kwargs):
         contact_id = kwargs['pk']
         contact = get_object_or_404(Contact, pk=contact_id)
-        contact.first_name = self.request.POST['first_name']
-        contact.save()
+        form = ContactForm(contact)
+
+
+    def post(self, *args, **kwargs):
+        form = ContactForm(self.request.POST)
+        if form.is_valid():
+            contact_id = kwargs['pk']
+            contact = get_object_or_404(Contact, pk=contact_id)
+            contact.first_name = form.cleaned_data['first_name']
+            contact.last_name = form.cleaned_data['first_name']
+            contact.address = form.cleaned_data['address']
+            contact.iban = form.cleaned_data['iban']
+            contact.bic = form.cleaned_data['bic']
+            contact.bank_name = form.cleaned_data['bank_name']
+            contact.phone = form.cleaned_data['phone']
+            contact.email = form.cleaned_data['email']
+            contact.remark = form.cleaned_data['remark']
+            contact.save()
         return HttpResponseRedirect(reverse('dkapp:contact', args=(contact_id,)))
