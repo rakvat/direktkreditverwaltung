aa2054842c8408630b67f340f567e05567121b18
rakvat <rakvat@users.noreply.github.com>
2020-09-05 12:32:08 +0200

    add bootstrap and navbar

--- dkverwaltung/settings.py
+++ dkverwaltung/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# needed for pm collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
